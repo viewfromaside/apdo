1b0c666ffe31da82571bdf92cdc9870c58d69870
side <viewfromaside@gmail.com>
2025-08-17 21:32:25 -0300

    [core] create scalable BaseEntity for reusable models

--- src/app/services/models/base.ts
+++ src/app/services/models/base.ts
@@ -5,6 +5,22 @@ export interface IBase {
   updatedAt: Date;
   deletedAt: Date;
 
-  getObjectForCreate: () => Record<string, any>;
-  getObjectForEdit: () => Record<string, any>;
+  getObjectForCreate?: () => Record<string, any>;
+  getObjectForEdit?: () => Record<string, any>;
+}
+
+export class BaseEntity implements IBase {
+  public id: string;
+  public createdBy: string;
+  public createdAt: Date;
+  public updatedAt: Date;
+  public deletedAt: Date;
+
+  constructor(obj?: Partial<IBase>) {
+    this.id = obj?.id || "";
+    this.createdBy = obj?.createdBy || "";
+    this.createdAt = obj?.createdAt || new Date();
+    this.updatedAt = obj?.updatedAt || new Date();
+    this.deletedAt = obj?.deletedAt || new Date();
+  }
 }
--- src/app/services/models/note.ts
+++ src/app/services/models/note.ts
@@ -1,7 +1,20 @@
-import { IBase } from "./base";
+import { BaseEntity, IBase } from "./base";
 
-export interface Note extends IBase {
+export interface INote extends IBase {
   title: string;
   content: string;
   favorite: boolean;
+}
+
+export class Note extends BaseEntity implements INote {
+  public title: string;
+  public content: string;
+  public favorite: boolean;
+
+  constructor(obj: Partial<INote & IBase>) {
+    super();
+    this.title = obj.title || "";
+    this.content = obj.content || "";
+    this.favorite = obj.favorite || false;
+  }
 }
