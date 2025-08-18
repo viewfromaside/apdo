ce3a821be694c1e3ec7526134a0c1e7400751428
side <viewfromaside@gmail.com>
2025-08-17 22:28:17 -0300

    [ui] toast now works on request

--- src/app/services/requests/base.ts
+++ src/app/services/requests/base.ts
@@ -1,5 +1,6 @@
 import { IBase } from "@/app/services/models";
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+import { toast } from "sonner";
 
 export class BaseRequest<T extends IBase> {
   protected url: string;
@@ -14,34 +15,90 @@ export class BaseRequest<T extends IBase> {
   }
 
   async sendCreate(data: Partial<T>): Promise<T> {
-    const { data: res } = await this.client.post<T>(
+    const { data: res, status } = await this.client.post<T>(
       "",
       data.getObjectForCreate!()
     );
+    if (status >= 200 && status < 300) {
+      toast.success("Success!", {
+        description: "The operation completed successfully.",
+        duration: 3000,
+      });
+    } else {
+      toast.error("Error", {
+        description: "Internal error, please contact support.",
+        duration: 5000,
+      });
+    }
+
     return res;
   }
 
   async sendRemove(id: string): Promise<boolean> {
     const res = await this.client.delete(`/${id}`);
-    return res.status >= 200 && res.status < 300;
+    let hasSuccess = res.status >= 200 && res.status < 300;
+    if (hasSuccess) {
+      toast.success("Success!", {
+        description: "The operation completed successfully.",
+        duration: 3000,
+      });
+    } else {
+      toast.error("Error", {
+        description: "Internal error, please contact support.",
+        duration: 5000,
+      });
+    }
+    return hasSuccess;
   }
 
   async sendEdit(id: string, data: Partial<T>): Promise<T> {
-    const { data: res } = await this.client.put<T>(
+    const { data: res, status } = await this.client.put<T>(
       `/${id}`,
       data.getObjectForEdit!()
     );
+
+    if (status >= 200 && status < 300) {
+      toast.success("Success!", {
+        description: "The operation completed successfully.",
+        duration: 3000,
+      });
+    } else {
+      toast.error("Error", {
+        description: "Internal error, please contact support.",
+        duration: 5000,
+      });
+    }
     return res;
   }
 
   async sendFindMany(params?: Record<string, any>): Promise<T[]> {
-    const { data: res } = await this.client.get<T[]>("", { params });
+    const { data: res, status } = await this.client.get<T[]>("", { params });
+
+    if (status >= 200 && status < 300) {
+    } else {
+      toast.error("Error", {
+        description: "Internal error, please contact support.",
+        duration: 5000,
+      });
+    }
     return res;
   }
 
   async sendFindOne(id: string): Promise<T | null> {
     try {
-      const { data: res } = await this.client.get<T>(`/${id}`);
+      const { data: res, status } = await this.client.get<T>(`/${id}`);
+
+      if (status >= 200 && status < 300) {
+        toast.success("Success!", {
+          description: "The operation completed successfully.",
+          duration: 3000,
+        });
+      } else {
+        toast.error("Error", {
+          description: "Internal error, please contact support.",
+          duration: 5000,
+        });
+      }
       return res;
     } catch (err: any) {
       if (err.response?.status == 404) return null;
