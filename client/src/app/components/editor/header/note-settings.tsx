9a207f52c61556661216e5c71f4638843e5c7665
side <viewfromaside@gmail.com>
2025-09-04 23:11:30 -0300

    [ui] more fixes

--- client/src/app/components/editor/header/note-settings.tsx
+++ client/src/app/components/editor/header/note-settings.tsx
@@ -1,13 +1,17 @@
+import { verifyItsLogged } from "@/store/user";
 import { Ellipsis } from "lucide-react";
 import Link from "next/link";
 import { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 
 export const NoteSettings = ({ className, ...props }: ComponentProps<"a">) => {
+  const isLogged = verifyItsLogged();
   return (
     <Link
       className={twMerge(
-        "px-2 cursor-pointer hover:opacity-60 duration-200 w-fit select-none font-mono py-1 text-[12px] tracking-wide bg-gray-500/20 rounded-md h-fit",
+        `${
+          !isLogged && "hidden"
+        } px-2 cursor-pointer hover:opacity-60 duration-200 w-fit select-none font-mono py-1 text-[12px] tracking-wide bg-gray-500/20 rounded-md h-fit`,
         className
       )}
       href={{ pathname: props.href }}
